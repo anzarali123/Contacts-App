420e216e2e7d35a8cc2d0728c4767bc3a1e252ec
Anzar Ali <anzarali150799@gmail.com>
2022-12-12 18:27:47 +0530

    added final touches

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const contactRouter = require("./routers/contact.router");
 const messageRouter = require("./routers/message.router");
 const { notFound, errorHandler } = require("./middlewares/error.middleware");
 const cors = require("cors");
+const path = require("path");
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
 
@@ -23,6 +25,22 @@ app.use(express.json());
 // routes
 app.use("/api/contact", contactRouter);
 app.use("/api/message", messageRouter);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname1, "..", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // error handling middlewares
 app.use(notFound);
